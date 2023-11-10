b923ea26487141a8c62811cc50e6785b26396232
Szent-Györgyi Technikum <szgyvs2020@gmail.com>
2023-11-10 11:19:14 +0100

    Add project files.

---
+++ feleviEredmeny/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace feleviEredmeny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tanulo> tanulok = new List<Tanulo>();
+            //1. feladat Írjunk függvényt, ami kiszámítja a jegyek átlagát tanulónként.Írjunk függvényt, ami kiszámítja az osztályátlagot, illetve a tantárgyankénti átlagot is.
+            var sr = new StreamReader(
+                path: @"..\..\..\src\forras.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                sr.ReadLine();
+                var sor = new Tanulo(sr.ReadLine());
+                tanulok.Add(sor);
+            }
+
+            double yes = 0;
+            foreach (var tanulo in tanulok)
+            {
+                Console.WriteLine($"{tanulo.ToString()} átlag: {tanulo.atlag(yes)}");
+            }
+
+
+            //2. feladat: Programozás gyakorlatból megbukottak adatainak kiiratása.
+            //3. feladat: Írjunk függvényt, amivel keressük meg az első olyan embert, akinek hármasa van angol nyelvből, majd írjuk ki az adatait.
+            //4. feladat: Kérjük be a felhasználótól, melyik tanuló legjobb jegyét szeretné megtudni.
+            //Írjuk ki az adott tanuló legjobb eredményét függvénnyel. Szorgalmi: Kezeljük a lehetséges felhasználói hibát is.
+            //5. feladat: Egy új fájlba írjuk ki a fenti tanuló nevét és oktatási azonosítóját.
+        }
+
+        public double osztalyAtlag(double atlag)
+        {
+
+            return atlag;
+        }
+    }
+}
---
+++ feleviEredmeny/Tanulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace feleviEredmeny
+{
+    class Tanulo
+    {
+        public string Diak { get; set; }
+        public double Oktazonosito { get; set; }
+        List<int> Lista { get; set; }
+
+
+        public Tanulo(string adatok)
+        {
+            string[] adat = adatok.Split("\t");
+            Diak = adat[0];
+            Oktazonosito = Convert.ToDouble(adat[1]);
+
+            Lista = new List<int>();
+            for (int i = 2; i < adat.Length; i++)
+            {
+                Lista.Add(Convert.ToInt32(adat[i]));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Diák neve {Diak}, azonosítója: {Oktazonosito}";
+        }
+        
+        public double atlag(double atlag)
+        {
+            foreach (var tanulo in Lista)
+            {
+                atlag += tanulo;
+            }
+            return atlag / Lista.Count;
+        }
+
+    }
+}
